777cc99edec7bbccf192a8fe8d3c59ba507dffca
TobyX73 <tobybecerra04@gmail.com>
2025-01-10 12:28:43 -0300

    Fix errors in the voidTrader File

--- Proyecto Bot Warframe - py/Eventos/earthRotation.py
+++ Proyecto Bot Warframe - py/Eventos/earthRotation.py
@@ -5,6 +5,9 @@ url = 'https://api.warframestat.us/pc/earthCycle/'
 response = requests.get(url)
 data = json.loads(response.text)
 
+
+#En este bloque de funcion lo unico que hago es checkear si el parametro 'isDay'
+#es true o false para dar su respectivo mensaje con el tiempo que tardara en rotar al siguiente
 if isinstance (data, dict):
     if data.get ('isDay', False):
         print ("La rotacion de la tierra esta en Dia")
--- Proyecto Bot Warframe - py/Eventos/voidTrader.py
+++ Proyecto Bot Warframe - py/Eventos/voidTrader.py
@@ -14,11 +14,14 @@ if isinstance (data, dict):
       print ('El comerciante del Vacío está activo')
       print (f"Lugar: {data.get('location','Desconocido')}")
       print (f"Se va en: {data.get('endString','Desconocido')}")
-      for item in ('inventory', []):
-         print (f"Nombre: {item.get('item', 'Desconocido')}")
-         print (f"Precio en Ducados: {item.get('ducats', 'Desconocido')}")
-         print (f"Precio en Créditos: {item.get('credits', 'Desconocido')}")
-         print ("---------------------------")
+      print ("Items que trajo: ")
+      print("-------------------------")
+      for item in data.get('inventory', []):
+         if isinstance(item, dict):
+          print (f"Nombre: {item.get('item', 'Desconocido')}")
+          print (f"Precio en Ducados: {item.get('ducats', 'Desconocido')}")
+          print (f"Precio en Créditos: {item.get('credits', 'Desconocido')}")
+          print ("---------------------------")
   else:
      print (f"El comerciante del Vacío no está activo, vendrá en: {data.get('startString', 'Desconocido')}")
 else: 
