c409d38f88ed03774a789a71fce8040e40874f20
TobyX73 <tobybecerra04@gmail.com>
2025-01-14 17:44:45 -0300

    Add the initial message bot and the dotenv python config

--- Proyecto Bot Warframe - py/main.py
+++ Proyecto Bot Warframe - py/main.py
@@ -1,1 +1,15 @@
+import telebot
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+
+TelegramToken = os.getenv("TelegramToken")
+bot = telebot.TeleBot(TelegramToken) 
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
+	
+
+bot.polling()
