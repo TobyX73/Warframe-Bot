27398d3cf1eaa7d450ce9f050c59da1c08eadbb3
TobyX73 <tobybecerra04@gmail.com>
2025-01-14 19:47:28 -0300

    Added the /Relics function to the bot

--- Proyecto Bot Warframe - py/Eventos/fissure.py
+++ Proyecto Bot Warframe - py/Eventos/fissure.py
@@ -5,21 +5,24 @@ url = "https://api.warframestat.us/pc/fissures"
 response = requests.get(url)
 data = json.loads(response.text)
 
-# Usé la funcion insinstance para comprobar si "data" es una lista, ya que es caso de que sea otra cosa va a tira error
-#Luego entra a un ciclo for donde cada fisura dentro de esta lista "data" imprime los datos de las fisuras al momento con el .get
-# Iterar sobre los datos y extraer información relevante
-if isinstance(data, list):
-    for fissure in data:
-        print(f"Relic type: {fissure.get('tier', 'Unknown')}")
-        print(f"Node: {fissure.get('node', 'Unknown')}")
-        print(f"Mission type: {fissure.get('missionType', 'Unknown')}")
-        print(f"Status: {fissure.get('active', 'Unknown')}")
-        print(f"Required level: {fissure.get('enemyLevels', 'Not specified')}")
-        isSteelPath = fissure.get('isHard', False)  
-        print(f"Steel Path: {'Yes' if isSteelPath else 'No'}")
-        print("-------------------")
-else:
-    print("The data format has changed or no fissures were found.")
-
+# Función que retorna toda la información de las fisuras
+# Función que retorna toda la información de las fisuras
+def Fissures():
+    fissuresInfo = []  # Array para almacenar cada fisura
+    if isinstance(data, list):
+        for fissure in data:
+            isSteelPath = fissure.get('isHard', False)
+            fissure_info = (
+                f"Relic type: {fissure.get('tier', 'Unknown')} \n" +
+                f"Node: {fissure.get('node', 'Unknown')}  \n" +
+                f"Mission type: {fissure.get('missionType', 'Unknown')} \n" +
+                f"Status: {fissure.get('active', 'Unknown')} \n" +
+                f"Steel Path: {'Yes' if isSteelPath else 'No'} \n" +
+                "-------------------"
+            )
+            fissuresInfo.append(str(fissure_info))  # Agregar la información de cada fisura a la lista
+        return "\n".join(fissuresInfo)  # Devuelve toda la lista unida de fisuras
+    else:
+        return "The data format has changed or no fissures were found."
 
 
--- Proyecto Bot Warframe - py/main.py
+++ Proyecto Bot Warframe - py/main.py
@@ -2,14 +2,37 @@ import telebot
 from dotenv import load_dotenv
 import os
 load_dotenv()
+from Eventos.fissure import Fissures
 
 
 TelegramToken = os.getenv("TelegramToken") #Esta linea lo que hace es que trae el Token desde el dotenv
 bot = telebot.TeleBot(TelegramToken) #Con esta linea conectamos el bot a Telegram
 
-@bot.message_handler(content_types= ["text", "sticker"])
+#Lo que hace esta parte del script es que toma cualquier mensaje que no tenga "/", entonces manda el mensaje de inicio.
+@bot.message_handler(func=lambda message: not message.text.startswith('/'), content_types=["text", "sticker"])
 def send_welcome(message):
-	bot.reply_to(message, "Welcome Operator! I am Ordis, your personal assistant. I can help you with the current rotations of different missions, here everything I am able to perform: \n /Relics to see the active fissures on the map. \n /Rotation to see the Night/Day cycle of the Earth. \n /VoidTrader To see the things that brings the Void Trader if it is available. \n /StatusWorlds To see the statuses of the 3 important open worlds.")
-	
+    bot.reply_to( message,
+        "Welcome Operator! I am Ordis, your personal assistant. I can help you with the current rotations of different missions. Here is everything I can perform: \n"
+        "/Relics - See the active fissures on the map.\n"
+        "/Rotation - See the Night/Day cycle of the Earth.\n"
+        "/VoidTrader - See the things the Void Trader brings, if available.\n"
+        "/StatusWorlds - See the statuses of the three important open worlds."
+    )
+    
+
+#Comando de Eventos 
+@bot.message_handler(commands=["Relics", "Relic", "relics", "relic"])
+def FissureVoid(message):
+    FissuresInfo = Fissures() 
+    bot.reply_to(message, FissuresInfo)
+
+ 
+@bot.message_handler(func=lambda message: message.text.startswith('/'))
+def unknown_command(message):
+    bot.reply_to(message, "Sorry, Operator, I do not recognize that command. Please try one of the available commands.")
+
+
+
+
 
 bot.polling() #Con esta inicializamos el bot
